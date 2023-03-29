4d528f41abcd554179703b6c392d8fe4b904cbeb
Эдуард Булак <bulakea@mail.ru>
2023-03-30 00:33:01 +0300

    feat: add new tests

--- RsSchool-1st/Tests.test.tsx
+++ RsSchool-1st/Tests.test.tsx
@@ -1,6 +1,8 @@
 import About from "./src/About/About";
 import Form from "./src/Form/Form";
 import Error from "./src/Error/Error";
+import Search from "./src/Search/Search";
+import Cards from "./src/Cards/Cards";
 import { describe, expect, it } from "vitest";
 import "@testing-library/jest-dom";
 import React from "react";
@@ -58,4 +60,51 @@ describe("Name Input event", () => {
       fireEvent.change(nameInput, { target: { value: "something else" } });
       expect(nameInput.value).toBe("something else");
     };
+});
+
+describe("Search Component", () => {
+  afterEach(() => {
+    localStorage.removeItem("inputValue");
+  });
+
+  it("renders a div with class 'Search__wrapper'", () => {
+    const { container } = render(<Search />);
+    expect(container.querySelector(".Search__wrapper")).toBeInTheDocument();
+  });
+});
+
+describe("Cards Component", () => {
+  const testCard = {
+    title: "Test Product",
+    brand: "Test Brand",
+    price: 100,
+    images: ["https://test.com/image.jpg"],
+    id: 1,
+    description: "An apple mobile which is nothing like apple",
+    discountPercentage: 12.96,
+    rating: 4.69,
+    category: "smartphones",
+    thumbnail: "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
+    stock: 4.69,
+  };
+
+  it("renders the card image", () => {
+    const { container } = render(<Cards card={testCard} />);
+    expect(container.querySelector(".card__image")).toBeInTheDocument();
+  });
+
+  it("renders the card title", () => {
+    const { getByText } = render(<Cards card={testCard} />);
+    expect(getByText("Test Product")).toBeInTheDocument();
+  });
+
+  it("renders the card brand", () => {
+    const { getByText } = render(<Cards card={testCard} />);
+    expect(getByText("Test Brand")).toBeInTheDocument();
+  });
+
+  it("renders the card price", () => {
+    const { getByText } = render(<Cards card={testCard} />);
+    expect(getByText("100$")).toBeInTheDocument();
+  });
 });
